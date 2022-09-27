36dd36cdc747a2c7be7d7e136250f56415cda167
Heider Oliveira <heider.a@hotmail.com>
2022-09-27 15:23:57 -0300

    Criação dos Métodos na Classe Livro

--- src/livraria/Livro.java
+++ src/livraria/Livro.java
@@ -4,4 +4,16 @@ public class Livro {
 
 	private String autor;
 	private String titulo;
+
+	public String exibirTipoLivro() {
+		return "";
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
 }
